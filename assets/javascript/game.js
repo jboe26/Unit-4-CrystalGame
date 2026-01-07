88cb8139d498698d2a09f611e0c2ae1539a34a49
Josh Boepple <joshrboepple@outlook.com>
2026-01-07 11:20:56 -0500

    cleaned up html, and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,101 +1,47 @@
-
 $(document).ready(function () {
 
-    var wins = 0;
-    var losses = 0;
-    var totalScore = 0;
+    let wins = 0;
+    let losses = 0;
+    let totalScore = 0;
 
-    var rubyValue = (Math.floor(Math.random() * 11) + 1);
-    var diamondValue = (Math.floor(Math.random() * 11) + 1);
-    var yellowValue = (Math.floor(Math.random() * 11) + 1);
-    var greenValue = (Math.floor(Math.random() * 11) + 1);
-    var currentGoal = (Math.floor(Math.random() * 101) + 19);
-    
-    $("#goal").html(currentGoal);
-    $("#totalScore").html(totalScore);
+    let rubyValue, diamondValue, yellowValue, greenValue, currentGoal;
 
-    console.log(currentGoal);
     function reset() {
-        rubyValue = (Math.floor(Math.random() * 11) + 1);
-        diamondValue = (Math.floor(Math.random() * 11) + 1);
-        yellowValue = (Math.floor(Math.random() * 11) + 1);
-        greenValue = (Math.floor(Math.random() * 11) + 1);
-        currentGoal = (Math.floor(Math.random() * 101) + 19);
-        $("#goal").html(currentGoal);
-        totalScore = 0;
-        $("#totalScore").html(totalScore);
-        
+        rubyValue = Math.floor(Math.random() * 11) + 1;
+        diamondValue = Math.floor(Math.random() * 11) + 1;
+        yellowValue = Math.floor(Math.random() * 11) + 1;
+        greenValue = Math.floor(Math.random() * 11) + 1;
 
+        currentGoal = Math.floor(Math.random() * 101) + 19;
+
+        $("#goal").text(currentGoal);
+        totalScore = 0;
+        $("#totalScore").text(totalScore);
+        $(".winorlose").text("");
     }
 
-    console.log(rubyValue)
-    console.log(diamondValue)
-    console.log(yellowValue)
-    console.log(greenValue)
+    reset();
 
+    function handleClick(value) {
+        totalScore += value;
+        $("#totalScore").text(totalScore);
 
-$(".crystal1").on("click", function(ev) {
-totalScore = totalScore + rubyValue
-$('#totalScore').text(totalScore);
-if (totalScore === currentGoal) {
-    wins++
-    $("#wins").text(wins);
-    $(".winorlose").html("You Won!");
-    reset();
-} if (totalScore > currentGoal) {
-    losses++
-    $("#losses").text(losses);
-    $(".winorlose").html("You Lose!");
-    reset(); 
-}
-});
+        if (totalScore === currentGoal) {
+            wins++;
+            $("#wins").text(wins);
+            $(".winorlose").text("You Won!");
+            reset();
+        } else if (totalScore > currentGoal) {
+            losses++;
+            $("#losses").text(losses);
+            $(".winorlose").text("You Lose!");
+            reset();
+        }
+    }
 
-$(".crystal2").on("click", function(ev) {
-totalScore = totalScore + diamondValue
-$('#totalScore').text(totalScore);
-if (totalScore === currentGoal) {
-    wins++
-    $("#wins").text(wins);
-    $(".winorlose").html("You Won!");
-    reset();
-} if (totalScore > currentGoal) {
-    losses++
-    $("#losses").text(losses);
-    $(".winorlose").html("You Lose!");
-    reset();
-}
-});
-
-$(".crystal3").on("click", function(ev) {
-totalScore = totalScore + yellowValue
-$('#totalScore').text(totalScore);
-if (totalScore === currentGoal) {
-    wins++
-    $("#wins").text(wins);
-    $(".winorlose").html("You Won!");
-    reset();
-} if (totalScore > currentGoal) {
-    losses++
-    $("#losses").text(losses);
-    $(".winorlose").html("You Lose!");
-    reset();
-}
-});
-
-$(".crystal4").on("click", function(ev) {
-totalScore = totalScore + greenValue
-$('#totalScore').text(totalScore);
-if (totalScore === currentGoal) {
-    wins++
-    $("#wins").text(wins);
-    $(".winorlose").html("You Won!");
-    reset();
-} if (totalScore > currentGoal) {
-    losses++
-    $("#losses").text(losses);
-    $(".winorlose").html("You Lose!");
-    reset();
-}
-});
+    $(".crystal1").on("click", () => handleClick(rubyValue));
+    $(".crystal2").on("click", () => handleClick(diamondValue));
+    $(".crystal3").on("click", () => handleClick(yellowValue));
+    $(".crystal4").on("click", () => handleClick(greenValue));
 
 });
